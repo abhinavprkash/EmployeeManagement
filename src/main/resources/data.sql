5dc4e19391d6b24f7ed8a40ba4e1610f821ce70c
Abhinav <abhip13th@gmail.com>
2023-06-08 16:11:55 +0530

    deleted the unnecassry files

--- src/main/resources/data.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO employee(first_name, last_name, email_id) VALUES
-('Abhi', 'P', 'abhip@ook.com'),
-('Abhinav', 'Prakash', 'abhinavprakash@ook.com'),
-('nav', 'kash', 'navkash@ook.com');
--- src/main/resources/schema.sql
+++
@@ -1,9 +0,0 @@
--- schema.sql
-
--- Employee table
-CREATE TABLE IF NOT EXISTS employee(
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(100) NOT NULL,
-    last_name VARCHAR(100) NOT NULL,
-    email_id VARCHAR(100) NOT NULL
-);
